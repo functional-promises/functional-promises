8e6fa934d701f522e8dfcf9215dec40119562aab
Dan Levy <dan@danlevy.net>
2018-02-02 09:47:45 -0700

    misc cleanup

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-const {FunctionalError}     = require('./modules/errors')
-const {isFunction, flatten} = require('./modules/utils')
-const {map, filter}         = require('./arrays')
-const {reduce}              = require('./arrays')
-const {find, findIndex}     = require('./arrays')
-const {thenIf, _thenIf}     = require('./conditional')
-const {tapIf}               = require('./conditional')
-const {listen}              = require('./events')
-const {chain, chainEnd}     = require('./monads')
-const {all, cast}           = require('./promise')
-const {reject, delay}       = require('./promise')
+const {FunctionalError}          = require('./modules/errors')
+const {isFunction, flatten}      = require('./modules/utils')
+const {map, filter, reduce}      = require('./arrays')
+const {find, findIndex}          = require('./arrays')
+const {thenIf, _thenIf, tapIf}   = require('./conditional')
+const {listen}                   = require('./events')
+const {chain, chainEnd}          = require('./monads')
+const {all, cast, reject, delay} = require('./promise')
 const FP = FunctionalPromise
 
 function FunctionalPromise(resolveRejectCB, unknownArgs) {
@@ -16,20 +13,14 @@ function FunctionalPromise(resolveRejectCB, unknownArgs) {
   if (unknownArgs != undefined) throw new Error('FunctionalPromise only accepts 1 argument')
   this._FP = {
     concurrencyLimit: 4,
-    hardErrorLimit: -1,
     promise: new Promise(resolveRejectCB),
   }
 }
 
 // FPromise Core Stuff
-FP.denodeify = FP.promisify = promisify
-FP.promisifyAll = promisifyAll
-FP.resolve = resolve
 FP.prototype.all = FP.all = all
 FP.prototype.cast = cast
 FP.prototype.reject = reject
-FP.prototype.tap = tap
-FP.prototype.then = then
 FP.prototype.delay = delay
 FP.delay = delay
 
@@ -52,9 +43,6 @@ FP.thenIf = _thenIf
 
 // Events Methods
 FP.prototype.listen = listen
-
-FP.unpack = unpack
-
 
 FP.prototype.addStep = function(name, args) {
   if (this.steps) this.steps.push([name, this, args])
@@ -119,13 +107,13 @@ FP.prototype.catchIf = function(condition, fn) {
   }))
 }
 
-function then(fn) {
+FP.prototype.then = function then(fn) {
   if (this.steps) return this.addStep('then', [...arguments])
   if (!isFunction(fn)) throw new FunctionalError('Invalid fn argument for `.then(fn)`. Must be a function. Currently: ' + typeof fn)
   return FP.resolve(this._FP.promise.then(fn))
 }
 
-function tap(fn) {
+FP.prototype.tap = function tap(fn) {
   if (this.steps) return this.addStep('tap', [...arguments])
   if (!isFunction(fn)) throw new FunctionalError('Invalid fn argument for `.tap(fn)`. Must be a function. Currently: ' + typeof fn)
   return FP.resolve(this._FP.promise.then(value => {
@@ -134,19 +122,19 @@ function tap(fn) {
   }))
 }
 
-function resolve(value) {
+FP.resolve = function resolve(value) {
   return new FP((resolve, reject) => {
     if (value && isFunction(value.then)) return value.then(resolve).catch(reject)
     resolve(value)
   })
 }
 
-function promisify(cb) {
+FP.promisify = function promisify(cb) {
   return (...args) => new FP((yah, nah) =>
     cb.call(this, ...args, (err, res) => err ? nah(err) : yah(res)))
 }
 
-function promisifyAll(obj) {
+FP.promisifyAll = function promisifyAll(obj) {
   if (!obj || !Object.getPrototypeOf(obj)) { throw new Error('Invalid Argument obj in promisifyAll(obj)') }
   return Object.getOwnPropertyNames(obj)
   .filter(key => typeof obj[key] === 'function')
@@ -156,14 +144,13 @@ function promisifyAll(obj) {
   }, obj)
 }
 
-function unpack() {
+FP.unpack = function unpack() {
   let resolve, reject, promise;
   promise = new Promise((yah, nah) => { resolve = yah; reject = nah })
   return { promise, resolve, reject }
 }
 
 module.exports = FunctionalPromise
-
 
 if (process && process.on) {
   process.on('uncaughtException', function (e) {
