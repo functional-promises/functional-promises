5dcf6b6a12e9c40c7c48b9c923957a8d110098cc
Dan Levy <dan@danlevy.net>
2019-10-26 23:30:02 -0600

    Fixed missing falsey check

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -1,12 +1,10 @@
 const utils = {
   isPromiseLike(p) { return !!(p && typeof p.then === 'function') },
   isFunction(fn) { return typeof fn === 'function' },
-  isEnumerable(list) { return list && Array.isArray(list) || typeof list[Symbol.iterator] === 'function' },
-
+  isEnumerable(list) { return list && Array.isArray(list) || list && typeof list[Symbol.iterator] === 'function' },
   flatten(arr) {
     if (!Array.isArray(arr)) throw new Error('Method `flatten` requires valid array parameter')
     return arr.reduce((results, item) => results.concat(Array.isArray(item) ? utils.flatten(item) : [item]), [])
   },
 }
-
 export default utils
